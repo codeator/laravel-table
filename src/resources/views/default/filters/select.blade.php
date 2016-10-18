fcf39b2fbdb754b5e7f90f71529a81aac352c847
Andrey Drozd <andrey@drozd.by>
2016-10-18 13:16:01 +0300

    select style update

--- src/resources/views/default/filters/select.blade.php
+++ src/resources/views/default/filters/select.blade.php
@@ -1,11 +1,13 @@
 <div class="form-group">
     <label>{{$label}}</label>
     <div class="row">
-        <select name="f_{{$name}}">
-            <option></option>
-            @foreach ($options as $key => $option)
-                <option value="{{$key}}" @if ($value == $key) selected @endif>{{$option}}</option>
-            @endforeach
-        </select>
+        <div class="col-xs-12">
+            <select name="f_{{$name}}" class="form-control c-select">
+                <option></option>
+                @foreach ($options as $key => $option)
+                    <option value="{{$key}}" @if ($value == $key) selected @endif>{{$option}}</option>
+                @endforeach
+            </select>
+        </div>
     </div>
 </div>
