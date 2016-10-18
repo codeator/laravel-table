4af0cbc1c82a6b78eea0891433136390e1a065a8
Andrey Drozd <andrey@drozd.by>
2016-10-18 15:15:24 +0300

    remove c-select class

--- src/resources/views/default/filters/select.blade.php
+++ src/resources/views/default/filters/select.blade.php
@@ -2,7 +2,7 @@
     <label>{{$label}}</label>
     <div class="row">
         <div class="col-xs-12">
-            <select name="f_{{$name}}" class="form-control c-select">
+            <select name="f_{{$name}}" class="form-control">
                 <option></option>
                 @foreach ($options as $key => $option)
                     <option value="{{$key}}" @if ($value == $key) selected @endif>{{$option}}</option>
