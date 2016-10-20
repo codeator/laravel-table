e4e793a0290d9ee8123d3578f4f458ae27671f43
codeator <codeator@gmail.com>
2016-10-20 16:46:45 +0300

    csv exporter bugfix

--- src/Exporter/CsvExporter.php
+++ src/Exporter/CsvExporter.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Codeator\Table\Exporter;
+
 use Codeator\Table\Exporter;
 
 /**
@@ -9,45 +10,60 @@ use Codeator\Table\Exporter;
  * Date: 14.10.16
  * Time: 11:43
  */
-class CsvExporter extends Exporter 
+class CsvExporter extends Exporter
 {
 
-    public function export($model) {
-        $results = $model->get($this->columns)->toArray();
+    public function export($model)
+    {
+        $modelColumns = DB::getSchemaBuilder()->getColumnListing($model->getModel()->getTable());
+        $columns = array_intersect($this->columns, $modelColumns);
 
-        $csv = $this->toCSV($results);
+        $results = $model->get($columns)->toArray();
 
-        header('Content-Disposition: attachment; filename="export.csv"');
-        header("Cache-control: private");
-        header("Content-type: application/force-download");
-        header("Content-transfer-encoding: binary\n");
-
-        echo $csv;
-
-        exit;
+        return $this->toCSV($results);
     }
 
-    function toCSV( array $fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
+    function toCSV(array $fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false)
+    {
         $delimiter_esc = preg_quote($delimiter, '/');
         $enclosure_esc = preg_quote($enclosure, '/');
 
         $outputString = "";
-        foreach($fields as $tempFields) {
-            $output = array();
-            foreach ( $tempFields as $field ) {
+        foreach ($fields as $tempFields) {
+            $output = [];
+            foreach ($tempFields as $field) {
                 if ($field === null && $nullToMysqlNull) {
                     $output[] = 'NULL';
                     continue;
                 }
 
                 // Enclose fields containing $delimiter, $enclosure or whitespace
-                if ( $encloseAll || preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field ) ) {
+                if ($encloseAll || preg_match("/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field)) {
                     $field = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $field) . $enclosure;
                 }
-                $output[] = $field." ";
+                $output[] = $field . " ";
             }
-            $outputString .= implode( $delimiter, $output )."\r\n";
+            $outputString .= implode($delimiter, $output) . "\r\n";
         }
-        return $outputString; }
+
+        $file = storage_path('export-'.time().'.csv');
+        file_put_contents($file, $outputString);
+
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+            unlink($file);
+            exit;
+        }
+    }
 
 }
