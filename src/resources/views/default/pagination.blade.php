36f6df490e5fb72538401c78e44531e5ea9d0161
codeator <codeator@gmail.com>
2016-10-21 16:11:08 +0300

    next prev buttons added

--- src/resources/views/default/pagination.blade.php
+++ src/resources/views/default/pagination.blade.php
@@ -12,6 +12,11 @@ $link_limit = 10; // maximum number of links (a little bit inaccurate, but will 
         <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
             <a href="{{ $paginator->url(1) }}">&laquo;&laquo;</a>
         </li>
+        @if ($paginator->onFirstPage())
+            <li class="disabled"><a href="#">&laquo;</a></li>
+        @else
+            <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
+        @endif
         @for ($i = 1; $i <= $paginator->lastPage(); $i++)
             <?php
             $half_total_links = floor($link_limit / 2);
@@ -30,6 +35,11 @@ $link_limit = 10; // maximum number of links (a little bit inaccurate, but will 
                 </li>
             @endif
         @endfor
+        @if ($paginator->hasMorePages())
+            <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
+        @else
+            <li class="disabled"><a href="#">&raquo;</a></li>
+        @endif
         <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
             <a href="{{ $paginator->url($paginator->lastPage()) }}">&raquo;&raquo;</a>
         </li>
